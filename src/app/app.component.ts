7a8fc3f74ce2a2fb9daa1bb4eb161f95b455cadf
Marco Wille <birdscur@outlook.dem>
2022-12-11 22:59:07 +0100

    added pictures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component(
+{
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularstart';
+export class AppComponent implements OnInit 
+{
+  images = ['1.jpg','2.jpg','3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  //function that starts when open app
+  ngOnInit()
+  {
+    this.updateImage() 
+  }
+
+
+  // a loop for changing pictures 
+  updateImage() 
+  {
+    setInterval(() =>
+    {
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length; // % = loop
+    this.showImage = false;
+
+    setTimeout(() => {
+      this.showImage = true;
+    }, 10);
+    },8000)
+  }
 }
